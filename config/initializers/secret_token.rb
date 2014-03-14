da0cd28fc89fb377c1d4d55853b2ef219e8e752d
pyemkey <py.emkey@gmail.com>
2014-03-14 13:10:26 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsStripeMembershipSaas::Application.config.secret_key_base = '6cb8e2bd31bced22344cfc6a8dbca928c92bf57c77b05155d0fff3e1dc5a88309003ecd214061dc4faf35dc48c3965db9ed6bdd965b814da447febcdfe2da3cb'
