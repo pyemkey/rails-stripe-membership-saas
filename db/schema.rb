c89992b14927d8cc722d8bcda2c402a8e21fe40b
pyemkey <py.emkey@gmail.com>
2014-03-14 17:04:35 +0100

    Update User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140314121917) do
+ActiveRecord::Schema.define(version: 20140314155709) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20140314121917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "customer_id"
+    t.string   "last_4_digits"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
