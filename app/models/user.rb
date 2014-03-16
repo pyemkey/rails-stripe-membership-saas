13b39a313bad6923cc82bb12003c8834817fbcfe
pyemkey <py.emkey@gmail.com>
2014-03-16 12:22:56 +0100

    Add simple validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,14 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, uniqueness: true, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true, length: { minimum: 8 }
+  validates :email, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   before_create :assign_role
+  
 
   def assign_role
     self.add_role :user if self.roles.first.nil?
