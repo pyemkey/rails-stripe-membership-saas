7e5d1f4058baf8f7e9667ce17d20037190cc6714
pyemkey <py.emkey@gmail.com>
2014-03-16 12:22:38 +0100

    Add action to update and destroy for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,37 @@
 class UsersController < ApplicationController
-
+  before_filter :authenticate_user!
   def index
     authorize! :index, @user, message: "Not authorized as an administrator"
     @users = User.all
   end
+
+  def update
+    authorize! :index, @user, message: "Not authorized as an administrator"
+    user = User.find(params[:id])
+    if user.update_attributes(secure_params)
+      redirect_to users_path
+    else
+      redirect_to users_path
+    end
+  end
+
+
+  def destroy
+    authorize! :index, @user, message: "Not authorized as an administrator"
+    user = User.find(params[:id])
+    if (current_user == user)
+      redirect_to users_path, error: "You cannot delete yourself"
+    else
+      user.delete
+      redirect_to users_path, notice: "User deleted"
+    end
+  end
+  private
+    def secure_params
+      if current_user && (current_user.has_role? :admin)
+        params.require(:user).permit(:role_ids, :name, :email, :password, :password_confirmation, :remember_me)
+      else
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me)
+      end
+    end
 end
