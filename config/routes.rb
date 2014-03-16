bcac95e57781501047a5dc75528c3817e1b74ad6
pyemkey <py.emkey@gmail.com>
2014-03-16 15:10:50 +0100

    add content pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsStripeMembershipSaas::Application.routes.draw do
+  get "content/silver"
+  get "content/gold"
+  get "content/platinum"
   devise_for :users
   root "home#index"
   resources :users
