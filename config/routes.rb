9b1e76df10310a8af5025bcd7b4f0e1bafe9d75a
pyemkey <py.emkey@gmail.com>
2014-03-17 08:05:55 +0100

    add registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RailsStripeMembershipSaas::Application.routes.draw do
   get "content/silver"
   get "content/gold"
   get "content/platinum"
-  devise_for :users
+  devise_for :users, controllers: { registration: 'registration'}
   root "home#index"
   resources :users
 end
